b5d7da5300c803c9197b86f7a47056b97167265e
Finn Glück <f.glueck@campus.tu-berlin.de>
2024-11-11 09:34:15 -0400

    Solve SQL Query Tasks

--- src/main/resources/SQL2Query04.sql
+++ src/main/resources/SQL2Query04.sql
@@ -10,3 +10,20 @@
 -- Points:
 -- 0.5
 --
+-- Zuerst berechnen wir die Preisgrenzen für alle MFGR-BRAND Kombinationen
+WITH price_boundaries AS (
+    SELECT MFGR,
+           BRAND,
+           MIN(RETAILPRICE) as min_price,
+           MAX(RETAILPRICE) as max_price
+    FROM PART
+    GROUP BY MFGR, BRAND
+)
+-- Dann joinen wir diese mit der ursprünglichen Tabelle
+SELECT p.MFGR, p.BRAND, p.NAME
+FROM PART p
+         JOIN price_boundaries pb
+              ON p.MFGR = pb.MFGR
+                  AND p.BRAND = pb.BRAND
+WHERE p.RETAILPRICE <= pb.min_price + 0.015 * (pb.max_price - pb.min_price)
+ORDER BY p.MFGR ASC, p.BRAND ASC, p.NAME DESC;
--- src/main/resources/SQL2Query07.sql
+++ src/main/resources/SQL2Query07.sql
@@ -8,3 +8,21 @@
 -- Points:
 -- 0.5
 --
+WITH supplier_counts AS (
+    SELECT
+        P.BRAND,
+        COUNT(DISTINCT S.SUPPKEY) as supplier_count
+    FROM PART P
+             JOIN PARTSUPP PS ON PS.PARTKEY = P.PARTKEY
+             JOIN SUPPLIER S ON S.SUPPKEY = PS.SUPPKEY
+    GROUP BY P.BRAND
+)
+SELECT BRAND
+FROM supplier_counts
+WHERE supplier_count = (
+    SELECT MAX(supplier_count)
+    FROM supplier_counts
+)
+ORDER BY BRAND ASC;
+
+
