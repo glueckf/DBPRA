7bf68bda2902b819b961700d4b2d463c4734b2da
Finn Glück <f.glueck@campus.tu-berlin.de>
2024-11-19 19:47:56 +0100

    Solve Query 06

--- src/main/resources/SQL3Query06.sql
+++ src/main/resources/SQL3Query06.sql
@@ -3,3 +3,14 @@
 -- Points:
 -- 1
 --
+ALTER TABLE CustomerContactData
+    ADD CONSTRAINT CHECK_Phone
+        CHECK (
+            SUBSTR(Phone, 1, 3) IN ('+45', '+46', '+47')
+            AND SUBSTR(Phone, 4, 1) = ' '
+            AND (CAST(SUBSTR(Phone, 5, 3) AS INT) BETWEEN 147 AND 158
+                OR CAST(SUBSTR(Phone, 5,3) AS INT) BETWEEN 171 AND 192)
+            AND SUBSTR(Phone, 8 ,1) = ' '
+            AND LENGTH(SUBSTR(Phone, 9)) BETWEEN 8 AND 10
+            );
+
