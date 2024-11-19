a6afe788e3c5ceeb7aa22c0dc6fe4a3c1e2053fe
Finn Glück <f.glueck@campus.tu-berlin.de>
2024-11-19 20:31:54 +0100

    Almost Solve Query 07 (currently stuck)

--- src/main/resources/SQL3Query07.sql
+++ src/main/resources/SQL3Query07.sql
@@ -1,5 +1,19 @@
 -- Task 7:
+--AND REGEXP_LIKE(SUBSTR(TwitterId, 2), '^[a-z0-9+-]+$')
 --
 -- Points:
 -- 1
 --
+
+ALTER TABLE CustomerContactData
+ADD CONSTRAINT Twitter_CHECK
+CHECK(
+    SUBSTR(TwitterID, 1, 1) = '@'
+    AND (
+        LENGTH(SUBSTR(TwitterId, 2)) <= 22
+        AND TRANSLATE(SUBSTR(TwitterId, 2),
+            'abcdefghijklmnopqrstuvwxyz0123456789*-',
+            'abcdefghijklmnopqrstuvwxyz0123456789*-') =
+                SUBSTR(TwitterId, 2)
+    )
+);
