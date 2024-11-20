7b3ae79fb437f5091421cd232619f407572e824b
Finn Glück <f.glueck@campus.tu-berlin.de>
2024-11-20 16:09:22 +0100

    Solve Task07

--- src/main/resources/SQL3Query07.sql
+++ src/main/resources/SQL3Query07.sql
@@ -1,19 +1,31 @@
 -- Task 7:
 --AND REGEXP_LIKE(SUBSTR(TwitterId, 2), '^[a-z0-9+-]+$')
 --
+/*
+ALTER TABLE CustomerContactData
+    ADD CONSTRAINT Twitter_CHECK
+        CHECK(
+            SUBSTR(TwitterID, 1, 1) = '@'
+                AND (
+                LENGTH(SUBSTR(TwitterId, 2)) <= 22
+                    AND TRANSLATE(SUBSTR(TwitterId, 2),
+                                  'abcdefghijklmnopqrstuvwxyz0123456789*-',
+                                  'abcdefghijklmnopqrstuvwxyz0123456789*-') =
+                        SUBSTR(TwitterId, 2)
+                )
+            );
+*/
 -- Points:
 -- 1
 --
-
 ALTER TABLE CustomerContactData
-ADD CONSTRAINT Twitter_CHECK
-CHECK(
-    SUBSTR(TwitterID, 1, 1) = '@'
-    AND (
-        LENGTH(SUBSTR(TwitterId, 2)) <= 22
-        AND TRANSLATE(SUBSTR(TwitterId, 2),
-            'abcdefghijklmnopqrstuvwxyz0123456789*-',
-            'abcdefghijklmnopqrstuvwxyz0123456789*-') =
-                SUBSTR(TwitterId, 2)
-    )
-);
+    ADD CONSTRAINT TwitterID_CHECK
+        CHECK(
+            TwitterID IS NULL
+                OR (
+                SUBSTR(TwitterID, 1, 1) = '@'
+                    AND LENGTH(SUBSTR(TwitterId, 2)) <= 22
+                    AND LOWER(SUBSTR(TwitterId, 2)) = SUBSTR(TwitterId, 2)
+                    AND TRANSLATE(SUBSTR(TwitterId, 2),'', 'abcdefghijklmnopqrstuvwxyz0123456789*-') = ''
+                )
+            );
