23fb8d38c263edecad532cd4839afe2463de33c9
Finn Glück <f.glueck@campus.tu-berlin.de>
2024-11-26 17:35:52 +0100

    Configure ConnectionConfig.java

--- src/main/java/de/tuberlin/dima/dbpra/config/ConnectionConfig.java
+++ src/main/java/de/tuberlin/dima/dbpra/config/ConnectionConfig.java
@@ -12,10 +12,10 @@ public class ConnectionConfig {
     public final static String DB2_URL = "jdbc:db2://gnu.dima.tu-berlin.de:50000/";
 
     // User Name für die Verbindung mit deiner Datenbank
-    public final static String DB2_USER = "";
+    public final static String DB2_USER = "grp33";
 
     // Passwort für die Verbindung mit deiner Datenbank
-    public final static String DB2_PW = "";
+    public final static String DB2_PW = "jtc8rtq2zjd4txn*TRG";
 
     // Name deiner Datenbank
     public final static String DB2_DB = "DBPRA";
